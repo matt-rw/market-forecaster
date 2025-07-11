2ea280289e975ef6f2451ff15f7814cea3677e69
Matt <mrwear12@gmail.com>
2025-07-10 20:12:51 -0700

    main training function

--- ml/tasks.py
+++ ml/tasks.py
@@ -1,12 +1,16 @@
 from celery import shared_task
 
+from ml.lstm_model.train import main
+# from ml.binary_model.train import main
+
 
 @shared_task
 def train_lstm():
-    seq_len = 20
-    train_ds, val_ds = load_datasets()
-    train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
-    val_loader = DataLoader(val_ds, batch_size=32)
+    # seq_len = 20
+    # train_ds, val_ds = load_datasets()
+    # train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
+    # val_loader = DataLoader(val_ds, batch_size=32)
 
-    model = LSTMRegressor(input_dim=5)
-    train_model(model, train_loader, val_loader, epochs=20)
+    # model = LSTMRegressor(input_dim=5)
+    # train_model(model, train_loader, val_loader, epochs=20)
+    main()
